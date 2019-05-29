773bfd4e58b3c90eedae4f2c331f5f8cf5952943
James Kan <james0077721@hotmail.com>
2019-05-28 22:25:40 -0400

    Binding Port Change for HTTPS

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 		endpoints.WebsocketHandler(c.Writer, c.Request, &connections)
 	})
 
-	go r.RunTLS(":443", "./MyCertificate.crt", "MyKey.key")
+	go r.RunTLS(":8443", "MyCertificate.crt", "MyKey.key")
 
 	if err = r.Run(":9090"); err != nil {
 		logrus.WithError(err).Fatal("Couldn't start server")
